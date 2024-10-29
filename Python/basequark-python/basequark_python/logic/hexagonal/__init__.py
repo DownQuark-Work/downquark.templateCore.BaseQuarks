c1af504058df4109c992d7f1b9d3b5cada5cbb49
mlnck <jack@melnick.>
2024-10-29 01:09:16 -0400

    done with hex docs

--- Python/basequark-python/basequark_python/logic/hexagonal/__init__.py
+++ Python/basequark-python/basequark_python/logic/hexagonal/__init__.py
@@ -1,9 +1,15 @@
 """
 See "Defining the core concepts":
     - https://netflixtechblog.com/ready-for-changes-with-hexagonal-architecture-b315ec967749
+
 The naming convention maps to what is described in the above link in this way:
     |    Ours   |   Theirs
     1. Handlers  -  Interactors
     2. Queries   - Repositories
     3. Objects -     Entities
+
+Below is an excellent article on _how_ the architecture should be used:
+    - https://jmgarridopaz.github.io/content/hexagonalarchitecture.html
+And this is an excellent article on _why_ the architecture should be used:
+    - https://scalastic.io/en/hexagonal-architecture-domain/
 """
