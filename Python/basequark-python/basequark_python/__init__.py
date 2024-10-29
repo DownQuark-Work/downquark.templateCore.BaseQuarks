0365728330323e71b8e1294ee05a0641f86b015a
mlnck <jack@melnick.>
2024-10-29 01:36:11 -0400

    spec stubbed

--- Python/basequark-python/basequark_python/__init__.py
+++ Python/basequark-python/basequark_python/__init__.py
@@ -1,7 +1,6 @@
 """
-    Description of project:
-The structure presented here is is greatly inspired by [Hexagonal Architecture](https://alistair.cockburn.us/hexagonal-architecture/),
-with a few tweaks along the way to make it better suit DowQuark's purposes.
+    Best practices recommend to have one _Driving Port_ consist of an assertion:
+Sutbbing here to show POC
 
 ---
 ![hexagonal overview](https://miro.medium.com/v2/resize:fit:1400/format:webp/1*NfFzI7Z-E3ypn8ahESbDzw.png)
