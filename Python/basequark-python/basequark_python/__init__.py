1726287328413875bd623df21ab158af19ca0c96
mlnck <jack@melnick.>
2024-10-29 00:01:39 -0400

    minor

--- Python/basequark-python/basequark_python/__init__.py
+++ Python/basequark-python/basequark_python/__init__.py
@@ -2,4 +2,7 @@
     Description of project:
 The structure presented here is is greatly inspired by [Hexagonal Architecture](https://alistair.cockburn.us/hexagonal-architecture/),
 with a few tweaks along the way to make it better suit DowQuark's purposes.
+
+---
+![hexagonal overview](https://miro.medium.com/v2/resize:fit:1400/format:webp/1*NfFzI7Z-E3ypn8ahESbDzw.png)
 """
