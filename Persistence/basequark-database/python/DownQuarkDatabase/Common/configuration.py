6c00b5ef786d97cca068eae7153e9f21daac32ae
mlnck <jack@melnick.>
2024-11-24 00:49:35 -0500

    more

--- Persistence/basequark-database/python/DownQuarkDatabase/Common/configuration.py
+++ Persistence/basequark-database/python/DownQuarkDatabase/Common/configuration.py
@@ -1,5 +1,7 @@
 """stores connection and other re-usable information"""
 
+import copy
+
 from .constants import DB_TYPES, DB_CREDENTIAL_KEYS
 
 DB_CONFIG_DEFAULT_VALUES = {  # set to typical local development defaults
@@ -17,19 +19,24 @@ DB_CONFIG_DEFAULT_VALUES = {  # set to typical local development defaults
 CREDENTIALS_ARANGO = ("root", "root", "localhost", "8529")
 CREDENTIALS_MARIA = ("username", "root", "localhost", "3666")
 CREDENTIALS_POSTGRES = ("postgres", "root", "localhost", "5432")
-DB_CONF = {
+DB_CONF_DEFAULT_VALUES = {
     "CREDENTIALS": {
         KEY_ARANGO: CREDENTIALS_ARANGO,
         KEY_MARIA: CREDENTIALS_MARIA,
         KEY_POSTGRES: CREDENTIALS_POSTGRES,
     }
 }
+DB_CONF = copy.deepcopy(DB_CONF_DEFAULT_VALUES)
 
 
 class DownQuarkDatabaseConfiguration:
     DB_CONFIG = {"1": "DB_CONFIG_DEFAULT_VALUES"}
 
     def reset(self):
+        DB_CONF["CREDENTIALS"][KEY_ARANGO] = ("goofy", "mouste", "1", "2")
+        print("pre-reset", DB_CONF)
+        DB_CONF["CREDENTIALS"] = copy.deepcopy(DB_CONF_DEFAULT_VALUES["CREDENTIALS"])
+        print("post-reset", DB_CONF)
         print("NOSET", self.DB_CONFIG)
         self.DB_CONFIG = DB_CONFIG_DEFAULT_VALUES
         print("resseted", self.DB_CONFIG)
@@ -38,5 +45,5 @@ class DownQuarkDatabaseConfiguration:
         print("Configue Database Information")
         print("NOSET", self.DB_CONFIG)
         print("DB_CONFIG_DEFAULT_VALUESINTERNAL", DB_CONFIG_DEFAULT_VALUES)
-        print("REFACTrored DB_CONF", DB_CONF["CREDENTIALS"][KEY_ARANGO][INDX_DB_HOST])
+        print("REFACTrored DB_CONF", DB_CONF["CREDENTIALS"])
         pass
