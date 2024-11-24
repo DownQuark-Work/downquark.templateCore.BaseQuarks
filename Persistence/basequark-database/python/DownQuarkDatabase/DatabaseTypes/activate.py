ab300a542c113758e95678281e57af3e2f9ec384
mlnck <jack@melnick.>
2024-11-24 06:48:49 -0500

    maria and python versioning working together

--- Persistence/basequark-database/python/DownQuarkDatabase/DatabaseTypes/activate.py
+++ Persistence/basequark-database/python/DownQuarkDatabase/DatabaseTypes/activate.py
@@ -1,12 +1,14 @@
 """Instantiate Successfully Configured Databases"""
 
 from DownQuarkDatabase.Common import configuration
+from DownQuarkDatabase.DatabaseTypes.MariaDB import relationaldb
 
 
 class ActiveDatabases:
     @staticmethod
     def determine_active_dbs():
         print("configuration.DB_CONF", configuration.DB_CONF)
+        relationaldb.print_dbs()
 
     def activate(self):
         print("initialize_active_databases")
