1fec3efe24e445de4f36fefadb60e0332016d02f
mlnck <jack@melnick.>
2024-11-24 20:07:02 -0500

    comment formatting

--- Persistence/basequark-database/python/DownQuarkDatabase/Catalog/database.py
+++ Persistence/basequark-database/python/DownQuarkDatabase/Catalog/database.py
@@ -6,8 +6,8 @@ class DatabaseCatalog:
 
     # def activate: # move activation calls here
     # def develop: # allows local/testing implementation that does not require an active database
-    # optional mock data can be passed as an argument which will be returned exactly as the database query result would be from the `process` method
-    # def query: # applies the query string to the desired database and passes the
-    # should probably be chainable: `.query("INSERT INTO...").query("SELECT * FROM...").process()
-    # https://github.com/faif/python-patterns/blob/master/patterns/behavioral/chaining_method.py
+    # -- optional mock data can be passed as an argument which will be returned exactly as the database query result would be from the `process` method
+    # def query: # applies the query string to the desired database and returns the results to the `process` function
+    # -- should probably be chainable: `.query("INSERT INTO...").query("SELECT * FROM...").process()
+    # ---- https://github.com/faif/python-patterns/blob/master/patterns/behavioral/chaining_method.py
     # def process: # will format the output from the query, then run the callback function (if exists) passing the formatted result(s) as `*args`
