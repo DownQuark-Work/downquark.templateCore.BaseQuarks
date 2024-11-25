38e8e6d95a18f07af862232b0b173d95ee9a3841
mlnck <jack@melnick.>
2024-11-24 20:29:36 -0500

    explanation stub doc

--- Persistence/basequark-database/python/DownQuarkDatabase/Catalog/database.py
+++ Persistence/basequark-database/python/DownQuarkDatabase/Catalog/database.py
@@ -8,6 +8,13 @@ class DatabaseCatalog:
     # def develop: # allows local/testing implementation that does not require an active database
     # -- optional mock data can be passed as an argument which will be returned exactly as the database query result would be from the `process` method
     # def query: # applies the query string to the desired database and returns the results to the `process` function
-    # -- should probably be chainable: `.query("INSERT INTO...").query("SELECT * FROM...").process()
+    # -- should probably be chainable: `query("INSERT INTO...").query("SELECT * FROM...").process()`
     # ---- https://github.com/faif/python-patterns/blob/master/patterns/behavioral/chaining_method.py
     # def process: # will format the output from the query, then run the callback function (if exists) passing the formatted result(s) as `*args`
+
+    # REMEMBER: any sort of customized logic is out of scope for this implementation
+    # -- `query("INSERT INTO...").MUTATING_RESULTS_IS_OUT_OF_SCOPE().query("SELECT * FROM...").process()
+    # ---- the query chain could be useful for short-handing multiple `input` statements before returning the un-mutated results of an aggregated `select` statement to be `process`ed.
+    # -- `process(); ANYTHING_ELSE_AFTERWARDS_IS_OUT_OF_SCOPE()
+    # ---- `process` executes a single callback function (if exists) - and that is it.
+    # ---- Any additional pipeline logic related to CQRS/event-sourcing/aggregations/etc (iterations, pub/sub, queue) is handled downstream and would require a new instance of this persistence module.
