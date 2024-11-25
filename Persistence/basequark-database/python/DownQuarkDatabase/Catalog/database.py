e1f9031b2a405e006ce236aa485802cf5cb31b86
mlnck <jack@melnick.>
2024-11-24 20:01:04 -0500

    stubbed content to finish catalog functionality

--- Persistence/basequark-database/python/DownQuarkDatabase/Catalog/database.py
+++ Persistence/basequark-database/python/DownQuarkDatabase/Catalog/database.py
@@ -4,7 +4,10 @@
 class DatabaseCatalog:
     """catalog of multiple static methods that are executed depending on an init parameter"""
 
+    # def activate: # move activation calls here
     # def develop: # allows local/testing implementation that does not require an active database
     # optional mock data can be passed as an argument which will be returned exactly as the database query result would be from the `process` method
     # def query: # applies the query string to the desired database and passes the
-    # def process: # will return the output from the query
+    # should probably be chainable: `.query("INSERT INTO...").query("SELECT * FROM...").process()
+    # https://github.com/faif/python-patterns/blob/master/patterns/behavioral/chaining_method.py
+    # def process: # will format the output from the query, then run the callback function (if exists) passing the formatted result(s) as `*args`
